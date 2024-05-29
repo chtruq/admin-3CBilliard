325f6c6dc20e34f38e1ac08afbfe26569452c9a5
chtruq <trunghcw@gmail.com>
2024-05-29 22:21:33 +0700

    setup

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SignIn() {
+  return (
+    <div>
+      <div className="items-center justify-center ">
+        <div>Connect-Cue-Community</div>
+        <div className="mx-auto">3C Billiard</div>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
